e3e0691604fb05a0ac66200f853c14f21ae33c55
max <max@mr.gy>
2013-08-24 17:04:13 +0200

    Fixed broken =NATURAL-NUMBER.

--- numerals.lisp
+++ numerals.lisp
@@ -8,14 +8,15 @@
 
 (defun =natural-number (&optional (radix 10))
   "Returns a parser which parses a natural number with {RADIX}."
-  (=result (parse-integer (=string-of (=digit radix)) :radix radix)))
+  (=let* ((numeric (=string-of (=digit radix))))
+    (=result (parse-integer numeric :radix radix))))
 
 (defun =integer-number (&optional (radix 10))
   "Returns a parser which parses an integer with {RADIX}."
-  (flet ((op () 
+  (flet ((=op () 
 	   (=plus (=let* ((_ (=character #\-)))
 		    (=result #'-))
 		 (=result #'identity))))
-    (=let* ((op (op))
+    (=let* ((op (=op))
 	    (n (=natural-number radix)))
       (=result (funcall op n)))))
